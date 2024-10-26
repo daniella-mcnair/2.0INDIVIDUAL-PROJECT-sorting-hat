5713e5ebc8b58626e79db28131c36dba54d058fa
Daniella McNair <daniella.mcnair495@gmail.com>
2024-10-25 20:41:14 -0500

    form works and pushes cards to dom, moved form to html

--- main.js
+++ main.js
@@ -1,94 +1,91 @@
-const cards = [
+const students = [
   { id: "1",
     imageUrl: "",
     name: "Liza",
-    house: "Ravenclaw", 
-    color: "blue",
+    house: "Gryffindor", 
   },
   { id: "2",
     imageUrl: "",
-    name: "Edward",
+    name: "Maxwell",
     house: "Ravenclaw", 
-    color: "blue",
   },
   { id: "3",
     imageUrl: "",
-    name: "Alice",
-    house: "Ravenclaw", 
-    color: "blue",
+    name: "Emily",
+    house: "Slytherin", 
   },
+  { id: "4",
+    imageUrl: "",
+    name: "Madaline",
+    house: "Hufflepuff", 
+  },
+
 ];
 
+const xStudents = [
+  { id: "1",
+    imageUrl: "",
+    name: "Jake",
+    house: "Ravenclaw", 
+  },
+  { id: "2",
+    imageUrl: "",
+    name: "Belle",
+    house: "Hufflepuff", 
+  },
+  { id: "3",
+    imageUrl: "",
+    name: "Belle",
+    house: "Hufflepuff", 
+  },
+  { id: "4",
+    imageUrl: "",
+    name: "Belle",
+    house: "Hufflepuff", 
+  },
+]
 
-const formApp = document.querySelector("#app");
+const houses = ["Gryffindor","Ravenclaw","Slytherin","Hufflepuff"];
 
-renderForm = () => {
-  let domstring = "";
-  domstring += 
-  `<div id = "app">
-    <div class="card">
-      <div class="card-body">
-
-      <form>
-      <div class="mb-3 card3">
-      <label for="exampleInputStudent" class="form-label">Enter Student Name</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-      <div id="emailHelp" class="form-text">We'll never share your house with anyone else.</div>
-      </div>
-    <div class="mb-3 form-check">
-    </div>
-    <button type="submit" class="btn btn-primary">Enroll</button>
-    </form>
+assignHouse = () => {
+  const randomIndex = Math.floor(Math.random() * houses.length);
+  const newHouse = houses[randomIndex];
+  return newHouse;
+}
 
 
+// const formApp = document.querySelector("#app");
 
-    </div>
-    
-    <div id = "students">
-    <div id ="enrolled" class = "card2">
-    </div>  
-    <div id ="expelled" class = "card2">
-    </div>  
-  </div>
-
-    </div>`
-    formApp.innerHTML = domstring
-
-const studentContainer = document.querySelector("#enrolled");
+const studentContainer = document.querySelector("#enrolled")
 
 const renderStudents = () => {
-  let cardstring = "";
-  cards.forEach((card) => {
-    cardstring +=
-    `<div class="card" style="width: 150px;">
-      <div class="card-header">${card.name}</div>
-      <img src=${card.imageUrl} class="card-img-top" alt="${card.id}">
+  let studentString = "";
+  students.forEach((student) => {
+    studentString +=
+    `<div class="card5" style="width: 150px;">
+      <div class="card-header">${student.house}</div>
+      <img src=${student.imageUrl} class="card-img-top" alt="${student.id}">
       <div class="card-body">
-       <button type="button" id="delete--${card.id}" class="delete-button btn btn-danger">Delete</button>
+       
       </div>
-      <div class=${card.house} "card-footer" >${card.house}</div>
+      <div class=${student.name} "card-footer" >${student.name}</div>
+      <button type="button" id="delete--${student.id}" class="delete-button btn btn-danger">Delete</button>
       </div>`;
   });
 
-  studentContainer.innerHTML = cardstring;
+  studentContainer.innerHTML = studentString;
+};  
 
-
-};
-
-
-    
-renderStudents();
-
+//render exisitng expelled students
 
 const studentContainer2 = document.querySelector("#expelled");
 
 const renderExpelled = () => {
   let expelstring = "";
-  cards.forEach((card) => {
+  xStudents.forEach((student) => {
     expelstring +=
-    `<div class="card" style="width: 150px;">
-      <div class="card-header">${card.name}</div>
-      <img src=${card.imageUrl} class="card-img-top" alt="${card.id}">
+    `<div class="card5" style="width: 150px;">
+      <div class="card-header">${student.name}</div>
       </div>`;
   });
 
@@ -98,71 +95,35 @@ const renderExpelled = () => {
 };
 
 
-    
-renderExpelled();
-
-
-  
-}
-
 
 const hideCard = () => {
   const card = document.getElementById("card");
   card.style.display = "none";
-  renderForm();
+  renderStudents();
+  renderExpelled();
 
 };
 
+const form = document.querySelector("#studentForm");
 
+const createStudent = (e) => {
+  e.preventDefault();
 
+  
+  const newStudent = {
+    id: students.length + 1,
+    name: document.querySelector("#name").value,
+    house: assignHouse(),
+  }
+
+  students.push(newStudent);
+  renderStudents();
+  form.reset();
+}
+
+form.addEventListener("submit", createStudent);
 
 
 
 const startApp = document.querySelector(".startButton");
 startApp.addEventListener("click", hideCard);
-
-
-
-
-
-// const renderHomeCard = () => {
-//   const cardApp = document.querySelector("#card");
-//   let domstring = "";
-//   domstring += `
-//   <div class="card" style="width: 30rem; height: 30rem;">
-//   <img src="../images/houses.jpg" class="card-img-top" alt="...">
-//   <div class="card-body">
-//     <h5 class="card-title">Welcome to Sorting Hat</h5>
-//     <which class="card-text">Which house will find you?</p>
-//     <button class="startButton btn btn-primary">Enter</button>
-//   </div>
-// </div>
-//   `
-//   cardApp.innerHTML = domstring;
-// }
-
-
-
-//   renderEvents = () => {
-//     for (card of cards) {
-//     let eventstring = "";
-//     eventstring += `<div class="card2" style="width: 19rem;">
-//      <img src=${card.imageUrl} class="card-img-top" alt="${card.id}">
-//     <div class="caerd-headr">${card.name}</div>
-//     <div class="card-body">
-//     <p class="card-text">${pet.specialSkill}</p>
-//     <button type="button" id="delete--${card.id}" class="delete-button btn btn-danger">Delete</button>
-//     </div>
-//     // <div class=${pet.type} "card-footer" >${pet.type}</div>
-//     </div>`
-
-//     // <button type="button" class="btn btn-danger" id="delete--${pets.id}>Delete</button>
-//   };
-//   house.innerHTML = eventstring;
-
-//   };
-
-
-
-// const startApp = document.getElementById(".startButton");
-// startApp.addEventListener("click", hideCard);
